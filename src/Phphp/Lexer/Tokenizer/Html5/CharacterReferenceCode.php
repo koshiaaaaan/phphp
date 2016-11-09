2f2750f79e3e0b29b575db62756f4a8210afc150
Daisuke Yabushita <yabushita@rook-jp.com>
2016-11-09 18:25:29 +0900

    wip

--- src/Phphp/Lexer/Tokenizer/Html5/CharacterReferenceCode.php
+++ src/Phphp/Lexer/Tokenizer/Html5/CharacterReferenceCode.php
@@ -3,36 +3,96 @@ namespace Phphp\Lexer\Tokenizer\Html5;
 
 class CharacterReferenceCode
 {
-    const REPLACEMENT_CHARACTER                         = 0xFFFD;  // REPLACEMENT CHARACTER
-    const EURO_SIGN                                     = 0x20AC;  // EURO SIGN (€)
-    const SINGLE_LOW_9_QUOTATION_MARK                   = 0x201A;  // SINGLE LOW-9 QUOTATION MARK (‚)
-    const LATIN_SMALL_LETTER_F_WITH_HOOK                = 0x0192;  // LATIN SMALL LETTER F WITH HOOK (ƒ)
-    const DOUBLE_LOW_9_QUOTATION_MARK                   = 0x201E;  // DOUBLE LOW-9 QUOTATION MARK („)
-    const HORIZONTAL_ELLIPSIS                           = 0x2026;  // HORIZONTAL ELLIPSIS (…)
-    const DAGGER                                        = 0x2020;  // DAGGER (†)
-    const DOUBLE_DAGGER                                 = 0x2021;  // DOUBLE DAGGER (‡)
-    const MODIFIER_LETTER_CIRCUMFLEX_ACCENT             = 0x02C6;  // MODIFIER LETTER CIRCUMFLEX ACCENT (ˆ)
-    const PER_MILLE_SIGN                                = 0x2030;  // PER MILLE SIGN (‰)
-    const LATIN_CAPITAL_LETTER_S_WITH_CARON             = 0x0160;  // LATIN CAPITAL LETTER S WITH CARON (Š)
-    const SINGLE_LEFT_POINTING_ANGLE_QUOTATION_MARK     = 0x2039;  // SINGLE LEFT-POINTING ANGLE QUOTATION MARK (‹)
-    const LATIN_CAPITAL_LIGATURE_OE                     = 0x0152;  // LATIN CAPITAL LIGATURE OE (Œ)
-    const LATIN_CAPITAL_LETTER_Z_WITH_CARON             = 0x017D;  // LATIN CAPITAL LETTER Z WITH CARON (Ž)
-    const LEFT_SINGLE_QUOTATION_MARK                    = 0x2018;  // LEFT SINGLE QUOTATION MARK (‘)
-    const RIGHT_SINGLE_QUOTATION_MARK                   = 0x2019;  // RIGHT SINGLE QUOTATION MARK (’)
-    const LEFT_DOUBLE_QUOTATION_MARK                    = 0x201C;  // LEFT DOUBLE QUOTATION MARK (“)
-    const RIGHT_DOUBLE_QUOTATION_MARK                   = 0x201D;  // RIGHT DOUBLE QUOTATION MARK (”)
-    const BULLET                                        = 0x2022;  // BULLET (•)
-    const EN_DASH                                       = 0x2013;  // EN DASH (–)
-    const EM_DASH                                       = 0x2014;  // EM DASH (—)
-    const SMALL_TILDE                                   = 0x02DC;  // SMALL TILDE (˜)
-    const TRADE_MARK_SIGN                               = 0x2122;  // TRADE MARK SIGN (™)
-    const LATIN_SMALL_LETTER_S_WITH_CARON               = 0x0161;  // LATIN SMALL LETTER S WITH CARON (š)
-    const SINGLE_RIGHT_POINTING_ANGLE_QUOTATION_MARK    = 0x203A;  // SINGLE RIGHT-POINTING ANGLE QUOTATION MARK (›)
-    const LATIN_SMALL_LIGATURE_OE                       = 0x0153;  // LATIN SMALL LIGATURE OE (œ)
-    const LATIN_SMALL_LETTER_Z_WITH_CARON               = 0x017E;  // LATIN SMALL LETTER Z WITH CARON (ž)
-    const LATIN_CAPITAL_LETTER_Y_WITH_DIAERESIS         = 0x0178;  // LATIN CAPITAL LETTER Y WITH DIAERESIS (Ÿ)
+    private $overrideTable = [
+        0x00    => 0xFFFD,  // REPLACEMENT CHARACTER
+        0x80    => 0x20AC,  // EURO SIGN (€)
+        0x82    => 0x201A,  // SINGLE LOW-9 QUOTATION MARK (‚)
+        0x83    => 0x0192,  // LATIN SMALL LETTER F WITH HOOK (ƒ)
+        0x84    => 0x201E,  // DOUBLE LOW-9 QUOTATION MARK („)
+        0x85    => 0x2026,  // HORIZONTAL ELLIPSIS (…)
+        0x86    => 0x2020,  // DAGGER (†)
+        0x87    => 0x2021,  // DOUBLE DAGGER (‡)
+        0x88    => 0x02C6,  // MODIFIER LETTER CIRCUMFLEX ACCENT (ˆ)
+        0x89    => 0x2030,  // PER MILLE SIGN (‰)
+        0x8A    => 0x0160,  // LATIN CAPITAL LETTER S WITH CARON (Š)
+        0x8B    => 0x2039,  // SINGLE LEFT-POINTING ANGLE QUOTATION MARK (‹)
+        0x8C    => 0x0152,  // LATIN CAPITAL LIGATURE OE (Œ)
+        0x8E    => 0x017D,  // LATIN CAPITAL LETTER Z WITH CARON (Ž)
+        0x91    => 0x2018,  // LEFT SINGLE QUOTATION MARK (‘)
+        0x92    => 0x2019,  // RIGHT SINGLE QUOTATION MARK (’)
+        0x93    => 0x201C,  // LEFT DOUBLE QUOTATION MARK (“)
+        0x94    => 0x201D,  // RIGHT DOUBLE QUOTATION MARK (”)
+        0x95    => 0x2022,  // BULLET (•)
+        0x96    => 0x2013,  // EN DASH (–)
+        0x97    => 0x2014,  // EM DASH (—)
+        0x98    => 0x02DC,  // SMALL TILDE (˜)
+        0x99    => 0x2122,  // TRADE MARK SIGN (™)
+        0x9A    => 0x0161,  // LATIN SMALL LETTER S WITH CARON (š)
+        0x9B    => 0x203A,  // SINGLE RIGHT-POINTING ANGLE QUOTATION MARK (›)
+        0x9C    => 0x0153,  // LATIN SMALL LIGATURE OE (œ)
+        0x9E    => 0x017E,  // LATIN SMALL LETTER Z WITH CARON (ž)
+        0x9F    => 0x0178,  // LATIN CAPITAL LETTER Y WITH DIAERESIS (Ÿ)
+    ];
+
+    private $sanitizeCode = 0xFFFD;
     
     private $code = 0;
+
+    /**
+     * @return boolean
+     */
+    public function isOverrideTarget()
+    {
+        return in_array($this->code, array_keys($this->overrideTable), true);
+    }
+
+    /**
+     * @throws \ErrorException
+     */
+    public function override()
+    {
+        if (!$this->isOverrideTarget()) {
+            throw new \ErrorException(sprintf('Does not in override table. `%d`', $this->code));
+        }
+        $this->code = $this->overrideTable[$this->code];
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isSanitizationTarget()
+    {
+        return ($this->code >= 0xD800 && $this->code <= 0xDFFF) || $this->code > 0x10FFFF;
+    }
+
+    /**
+     * @throws \ErrorException
+     */
+    public function sanitize()
+    {
+        if (!$this->isSanitizationTarget()) {
+            throw new \ErrorException(sprintf('Does not sanitize target. `%d`', $this->code));
+        }
+        $this->code = $this->sanitizeCode;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isInvalidTarget()
+    {
+        return ($this->code >= 0x0001 && $this->code <= 0x0008) ||
+                ($this->code >= 0x000D && $this->code <= 0x001F) ||
+                ($this->code >= 0x007F && $this->code <= 0x009F) ||
+                ($this->code >= 0xFDD0 && $this->code <= 0xFDEF) ||
+                in_array($this->code, [
+                    0x000B, 0xFFFE,  0xFFFF,  0x1FFFE, 0x1FFFF, 0x2FFFE, 0x2FFFF,
+                    0x3FFFE, 0x3FFFF, 0x4FFFE, 0x4FFFF, 0x5FFFE, 0x5FFFF, 0x6FFFE,
+                    0x6FFFF, 0x7FFFE, 0x7FFFF, 0x8FFFE, 0x8FFFF, 0x9FFFE, 0x9FFFF,
+                    0xAFFFE, 0xAFFFF, 0xBFFFE, 0xBFFFF, 0xCFFFE, 0xCFFFF, 0xDFFFE,
+                    0xDFFFF, 0xEFFFE, 0xEFFFF, 0xFFFFE, 0xFFFFF, 0x10FFFE, 0x10FFFF
+                ], true);
+    }
 
     /**
      * @param integer $code
@@ -44,6 +104,14 @@ class CharacterReferenceCode
         return $this;
     }
 
+    /**
+     * @return integer
+     */
+    public function get()
+    {
+        return $this->code;
+    }
+    
     /**
      * @param integer $value
      * @return CharacterReferenceCode
--- src/Phphp/Lexer/Tokenizer/Html5/State/NumericCharacterReferenceEnd.php
+++ src/Phphp/Lexer/Tokenizer/Html5/State/NumericCharacterReferenceEnd.php
@@ -10,37 +10,6 @@ use Phphp\Lexer\Tokenizer\Tokenizer;
  */
 class NumericCharacterReferenceEnd extends AbstractState
 {
-    private $characterReferenceCodeMap  = [
-        0x00    => CharacterReferenceCode::REPLACEMENT_CHARACTER,  // REPLACEMENT CHARACTER
-        0x80    => CharacterReferenceCode::EURO_SIGN,  // EURO SIGN (€)
-        0x82    => CharacterReferenceCode::SINGLE_LOW_9_QUOTATION_MARK,  // SINGLE LOW-9 QUOTATION MARK (‚)
-        0x83    => CharacterReferenceCode::LATIN_SMALL_LETTER_F_WITH_HOOK,  // LATIN SMALL LETTER F WITH HOOK (ƒ)
-        0x84    => CharacterReferenceCode::DOUBLE_LOW_9_QUOTATION_MARK,  // DOUBLE LOW-9 QUOTATION MARK („)
-        0x85    => CharacterReferenceCode::HORIZONTAL_ELLIPSIS,  // HORIZONTAL ELLIPSIS (…)
-        0x86    => CharacterReferenceCode::DAGGER,  // DAGGER (†)
-        0x87    => CharacterReferenceCode::DOUBLE_DAGGER,  // DOUBLE DAGGER (‡)
-        0x88    => CharacterReferenceCode::MODIFIER_LETTER_CIRCUMFLEX_ACCENT,  // MODIFIER LETTER CIRCUMFLEX ACCENT (ˆ)
-        0x89    => CharacterReferenceCode::PER_MILLE_SIGN,  // PER MILLE SIGN (‰)
-        0x8A    => CharacterReferenceCode::LATIN_CAPITAL_LETTER_S_WITH_CARON,  // LATIN CAPITAL LETTER S WITH CARON (Š)
-        0x8B    => CharacterReferenceCode::SINGLE_LEFT_POINTING_ANGLE_QUOTATION_MARK,  // SINGLE LEFT-POINTING ANGLE QUOTATION MARK (‹)
-        0x8C    => CharacterReferenceCode::LATIN_CAPITAL_LIGATURE_OE,  // LATIN CAPITAL LIGATURE OE (Œ)
-        0x8E    => CharacterReferenceCode::LATIN_CAPITAL_LETTER_Z_WITH_CARON,  // LATIN CAPITAL LETTER Z WITH CARON (Ž)
-        0x91    => CharacterReferenceCode::LEFT_SINGLE_QUOTATION_MARK,  // LEFT SINGLE QUOTATION MARK (‘)
-        0x92    => CharacterReferenceCode::RIGHT_SINGLE_QUOTATION_MARK,  // RIGHT SINGLE QUOTATION MARK (’)
-        0x93    => CharacterReferenceCode::LEFT_DOUBLE_QUOTATION_MARK,  // LEFT DOUBLE QUOTATION MARK (“)
-        0x94    => CharacterReferenceCode::RIGHT_DOUBLE_QUOTATION_MARK,  // RIGHT DOUBLE QUOTATION MARK (”)
-        0x95    => CharacterReferenceCode::BULLET,  // BULLET (•)
-        0x96    => CharacterReferenceCode::EN_DASH,  // EN DASH (–)
-        0x97    => CharacterReferenceCode::EM_DASH,  // EM DASH (—)
-        0x98    => CharacterReferenceCode::SMALL_TILDE,  // SMALL TILDE (˜)
-        0x99    => CharacterReferenceCode::TRADE_MARK_SIGN,  // TRADE MARK SIGN (™)
-        0x9A    => CharacterReferenceCode::LATIN_SMALL_LETTER_S_WITH_CARON,  // LATIN SMALL LETTER S WITH CARON (š)
-        0x9B    => CharacterReferenceCode::SINGLE_RIGHT_POINTING_ANGLE_QUOTATION_MARK,  // SINGLE RIGHT-POINTING ANGLE QUOTATION MARK (›)
-        0x9C    => CharacterReferenceCode::LATIN_SMALL_LIGATURE_OE,  // LATIN SMALL LIGATURE OE (œ)
-        0x9E    => CharacterReferenceCode::LATIN_SMALL_LETTER_Z_WITH_CARON,  // LATIN SMALL LETTER Z WITH CARON (ž)
-        0x9F    => CharacterReferenceCode::LATIN_CAPITAL_LETTER_Y_WITH_DIAERESIS,  // LATIN CAPITAL LETTER Y WITH DIAERESIS (Ÿ)
-    ];
-
     public function handle()
     {
         $tokenizer = $this->getTokenizer();
@@ -48,33 +17,25 @@ class NumericCharacterReferenceEnd extends AbstractState
         // Check the character reference code.
         $code   = $tokenizer->getCharacterReferenceCode();
 
-        if (in_array($code, array_keys($this->characterReferenceCodeMap), true)) {
+        // If that number is one of the numbers in the first column of the following table, then this is a parse error.
+        // Find the row with that number in the first column, and set the character reference code to the number in the
+        // second column of that row.
+        //
+        // https://html.spec.whatwg.org/multipage/syntax.html#numeric-character-reference-end-state
+        if ($code->isOverrideTarget()) {
             $tokenizer->error(Tokenizer::PARSE_ERROR);
-            $code = $this->characterReferenceCodeMap[$code];
-            $tokenizer->setCharacterReferenceCode($code);
+            $code->override();
         }
 
         // If the number is in the range 0xD800 to 0xDFFF or is greater than
         // 0x10FFFF, then this is a parse error. Set the character reference
         // code to 0xFFFD.
-        else if ($this->inRange($code, 0xD800, 0xDFFF) || $code > 0x10FFFF) {
+        else if ($code->isSanitizationTarget()) {
             $tokenizer->error(Tokenizer::PARSE_ERROR);
-            $tokenizer->setCharacterReferenceCode($code);
+            $code->sanitize();
         }
 
-        else if (
-            $this->inRange($code, 0x0001, 0x0008) ||
-            $this->inRange($code, 0x000D, 0x001F) ||
-            $this->inRange($code, 0x007F, 0x009F) ||
-            $this->inRange($code, 0xFDD0, 0xFDEF) ||
-            in_array($code, [
-                0x000B, 0xFFFE, 0xFFFF, 0x1FFFE, 0x1FFFF, 0x2FFFE, 0x2FFFF,
-                0x3FFFE, 0x3FFFF, 0x4FFFE, 0x4FFFF, 0x5FFFE, 0x5FFFF, 0x6FFFE,
-                0x6FFFF, 0x7FFFE, 0x7FFFF, 0x8FFFE, 0x8FFFF, 0x9FFFE, 0x9FFFF,
-                0xAFFFE, 0xAFFFF, 0xBFFFE, 0xBFFFF, 0xCFFFE, 0xCFFFF, 0xDFFFE,
-                0xDFFFF, 0xEFFFE, 0xEFFFF, 0xFFFFE, 0xFFFFF, 0x10FFFE, 0x10FFFF
-            ], true)
-        ) {
+        else if ($code->isInvalidTarget()) {
             $tokenizer->error(Tokenizer::PARSE_ERROR);
         }
 
